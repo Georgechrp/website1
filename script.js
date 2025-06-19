b5841f9663fb9f90363397184ec64173197c42b9
Georgechrp <christopoulosg2@gmail.com>
2025-06-19 21:26:49 +0300

    update

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ document.getElementById("langSwitcher").addEventListener("change", async functio
     if (data[lang] && data[lang][key]) el.textContent = data[lang][key];
   });
 });
+
+
+
+async function loadSiteData() {
+  try {
+    const res = await fetch("data/site.yml");
+    const yamlText = await res.text();
+    const siteData = jsyaml.load(yamlText);
+
+    document.getElementById("name").textContent = siteData.name;
+    document.getElementById("title").textContent = siteData.title;
+    document.getElementById("about").textContent = siteData.about_text;
+    document.getElementById("email").textContent = siteData.email;
+    document.getElementById("github").href = siteData.github;
+  } catch (error) {
+    console.error("Failed to load site.yml:", error);
+  }
+}
+
+loadSiteData();
